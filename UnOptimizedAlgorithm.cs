54b5e789e43844c1cee9bad2d52692fa76a32b3c
Ignis Vitae <13nastya310501@mail.ru>
2022-11-14 18:24:01 +0300

    renamed file and method

--- UnOptimizedAlgorithm.cs
+++ UnOptimizedAlgorithm.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace sorting_algorithm
 {
-    public class NonOptimizedAlgorithm: ISortingAlgorithm
+    public class UnOptimizedAlgorithm: ISortingAlgorithm
     {
         public void Sort (double[] items)
         {
