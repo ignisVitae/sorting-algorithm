25209bd202c5e0c7d0ed78584a5ca25afd27e474
Ignis Vitae <13nastya310501@mail.ru>
2022-11-14 16:09:32 +0300

    added nonoptimized algorithm

--- NonOptimizedAlgorithm.cs
+++ NonOptimizedAlgorithm.cs
@@ -9,14 +9,52 @@ namespace sorting_algorithm
     {
         public void Sort (double[] items)
         {
-            // TODO
+            double[] buffer = new double[items.Count()];
 
-            //BreakMerge();
+            BreakMerge(items, buffer, 0, items.Count() - 1);
         }
 
         private void BreakMerge (double[] items, double[] buffer, int minIndex, int maxIndex)
         {
-            // TODO
+            if (minIndex < maxIndex)
+            {
+                int midIndex = (minIndex + maxIndex) / 2;
+
+                BreakMerge(items, buffer, minIndex, midIndex);
+                BreakMerge(items, buffer, midIndex + 1, maxIndex);
+
+                int left = minIndex;
+                int right = midIndex + 1;
+
+                for (int i = minIndex; i <= maxIndex; i++)
+                {
+                    if (items[left] < items[right])
+                    {
+                        buffer[i] = items[left];
+                        left++;
+
+                        if (left > midIndex)
+                        {
+                            for (int j = right; j <= maxIndex; j++)
+                                buffer[i] = items[j];
+                        }
+                    }
+                    else
+                    {
+                        buffer[i] = items[right];
+                        right++;
+
+                        if (right > maxIndex && left <= midIndex)
+                        {
+                            for (int j = left; j <= midIndex; j++)
+                                buffer[i] = items[j];
+                        }
+                    }
+                }
+
+                for (int i = 0; i <= maxIndex; i++)
+                    items[i] = buffer[i];
+            }
         }
     }
 }
