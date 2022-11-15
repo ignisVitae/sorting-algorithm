b05ec4e1624b116b10b845e0ff1a8e2f7996429a
Ignis Vitae <13nastya310501@mail.ru>
2022-11-15 15:17:12 +0300

    added the use of sorting algorithm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using sorting_algorithm;
+
+double[] array = new double[24500];
+array.RandomFill();
+TimeSpan measureTime = Tracker.MeasureTime(() => new UnOptimizedAlgorithm().Sort(array));
+
+using (StreamWriter fileStream = new StreamWriter(@"C:\Users\13nas\Desktop\measureTime.txt", true))
+    fileStream.WriteLine(measureTime.TotalMilliseconds);
