4d6abf389c4e06772eb3d02c4e14a8868b3cfc91
Ignis Vitae <13nastya310501@mail.ru>
2022-11-19 14:29:00 +0300

    fix some bug IndexOutOfRange, added additional conditions for loop

--- OptimizedAlgorithm.cs
+++ OptimizedAlgorithm.cs
@@ -23,53 +23,61 @@ namespace sorting_algorithm
                 BreakMerge(items, buffer, minIndex, midIndex);
                 BreakMerge(items, buffer, midIndex + 1, maxIndex);
 
+                bool flag = true;
+                int buf = 0;
                 int left = minIndex;
                 int right = midIndex + 1;
 
-                for (int i = minIndex; i <= maxIndex; i++)
+                for (int i = minIndex, j = 0; i <= maxIndex; i++, j++)
                 {
                     if (items[left] < items[right])
                     {
-                        buffer[i] = items[left];
-                        ++left;
+                        buffer[j] = items[left];
+                        left++;
 
                         if (left > midIndex) break;
                     }
                     else
                     {
-                        buffer[i] = items[right];
-                        ++right;
+                        buffer[j] = items[right];
+                        right++;
 
-                        if (right > maxIndex)
+                        if (right > maxIndex && left == minIndex)
                         {
                             int condition = midIndex + 1;
-                            if (items.Count() % 2 == 0) condition = midIndex;
+                            if ((maxIndex - minIndex + 1) % 2 != 0) condition = midIndex;
 
-                            for (int k = maxIndex; k >= condition; k--)
-                                items[k] = items[k - condition];
+                            for (int k = maxIndex; k >= condition; k--, midIndex--)
+                                items[k] = items[midIndex];
 
-                            for (int k = 0; k < condition; k++)
-                                items[k] = buffer[k];
+                            int l = 0;
+                            if (!flag) l = minIndex - buf;
+                            for (int k = minIndex; k < condition; k++, l++)
+                                items[k] = buffer[l];
 
                             break;
                         }
                     }
 
-                    if (i == midIndex && left < midIndex && right < maxIndex)
+                    if (i == midIndex && flag)
                     {
-                        for (int k = midIndex + 1; k < right; k++)
-                            items[k] = items[k - left + 1];
+                        for (int k = midIndex + 1; k < right; k++, left++)
+                            items[k] = items[left];
 
-                        for (int k = 0; k <= midIndex; k++)
-                            items[k] = buffer[k];
+                        for (int k = minIndex, l = 0; k <= midIndex; k++, l++)
+                            items[k] = buffer[l];
+
+                        if (right > maxIndex) break;
+                        flag = false;
 
                         left = midIndex + 1;
+                        buf = midIndex + 1;
                         midIndex = (left + maxIndex) / 2;
+                        right = midIndex + 1;
+                        minIndex = left;
+                        j = -1;
                     }
                 }
-
-                for (int k = midIndex + 1; k <= maxIndex; k++)
-                    items[k] = buffer[k - midIndex - 1];
             }
         }
     }
